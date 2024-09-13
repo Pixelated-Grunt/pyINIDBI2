bbb342b7c6052eae4de543986c2312b1cba78e78
Pixelated Grunt <pixelatedgrunt4@gmail.com>
2024-09-13 23:04:00 +1000

    Update to include fully working code

--- pyinidbi2/pyinidbi2.py
+++ pyinidbi2/pyinidbi2.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime, timezone
 
 
@@ -5,48 +6,103 @@ class Section:
     def __init__(self, name: str) -> None:
         self.name = name
         self._content: dict[str, str] = {}
-        self._items_count = 0
 
     def __len__(self):
         return len(self._content)
 
+    def get(self, key: str) -> str | None:
+        try:
+            return self._content[key]
+        except KeyError:
+            return None
+
+    def keys(self) -> tuple[str, ...]:
+        return tuple(self._content.keys())
+
     def set(self, key: str, value: str) -> None:
         self._content[key] = value
-        self._items_count = len(self._content)
+
+    def remove(self, key: str, default: str | None = None) -> str | None:
+        try:
+            return self._content.pop(key, default)
+        except KeyError:
+            return None
 
     def as_string(self) -> str:
         content = f"[{self.name}]\n"
-        for k, v in self._content.items():
-            content += f'{k} = "{v}"\n'
-
+        if len(self._content) > 0:
+            for k, v in self._content.items():
+                content += f'{k}="{v}"\n'
         return content
 
 
 class INIFile:
-    def __init__(
-        self, db_name: str, filename: str, path: str | None = "db/"
-    ) -> None:
+    def __init__(self, db_name: str, filename: str, path: str | None = "db/") -> None:
         self.db_name = db_name
         self.filename = filename
         self.file_path = f"{path}{filename}"
         self._sections: list[Section] = []
+        if os.path.isfile(self.file_path):
+            self._load()
+            if len(self._sections) == 0:
+                self._sections.append(self._add_meta())
+        else:
+            self._sections.append(self._add_meta())
+        self._fd = open(self.file_path, "w")
+        self.write()
+
+    def __del__(self) -> None:
+        self.write()
+        self._fd.close()
+
+    @property
+    def sections(self) -> list[str]:
+        return [n.name for n in self._sections if n.name != "meta"]
+
+    @staticmethod
+    def _list_to_section(lines: list[str]) -> Section:
+        line = lines.pop(0)
+        name = line[line.find("[") + 1 : line.find("]")]
+        section = Section(name)
+
+        for line in lines:
+            idx = line.find("=")
+            if idx != -1:
+                key = line[:idx]
+                value = line[idx + 1 :].strip("\n")
+                section.set(key, value[1:-1])
+        return section
+
+    def _add_meta(self) -> Section:
         meta = Section("meta")
-        meta.set("db_name", db_name)
+        meta.set("db_name", self.db_name)
         meta.set(
             "create_date_utc",
             str(datetime.now(timezone.utc).replace(microsecond=0, tzinfo=None)),
         )
         meta.set("create_date_server", str(datetime.now().replace(microsecond=0)))
-        self.add(meta)
+        return meta
 
-    @property
-    def sections(self) -> list[str]:
-        return [n.name for n in self._sections]
+    def _load(self) -> None:
+        sec_list: list[str] = []
+        with open(self.file_path) as fd:
+            for line in fd.readlines():
+                if (line.find("[")) == 0:
+                    if len(sec_list) > 0:
+                        section = self._list_to_section(sec_list)
+                        self._sections.append(section)
+                        sec_list = sec_list[:0]
+                sec_list.append(line)
+
+        # last section that didn't get process before fd closed
+        if len(sec_list) > 0:
+            section = self._list_to_section(sec_list)
+            self._sections.append(section)
 
     def get(self, name: str) -> Section | None:
         try:
             idx = self.sections.index(name)
-            return self._sections[idx]
+            return self._sections[1:][idx]
         except ValueError:
             return None
 
@@ -58,7 +114,21 @@ class INIFile:
             self._sections.append(section)
         self.write()
 
-    def write(self) -> None:
-        with open(self.file_path, "w") as fd:
-            for section in self._sections:
-                _ = fd.write(section.as_string())
+    def remove(self, name: str) -> None:
+        section = self.get(name)
+        if section is not None:
+            self._sections = [x for x in self._sections if x.name != name]
+
+    def as_string(self) -> str:
+        output = ""
+        for s in self._sections:
+            output += s.as_string()
+        return output
+
+    def write(self, flush: bool = False) -> None:
+        _ = self._fd.seek(0)
+        _ = self._fd.truncate()
+        for section in self._sections:
+            _ = self._fd.write(section.as_string())
+        if flush:
+            self._fd.flush()
--- tests/test_pyinidbi2.py
+++ tests/test_pyinidbi2.py
@@ -7,17 +7,40 @@ from pyinidbi2.pyinidbi2 import INIFile, Section
 
 DBNAME = "db_name"
 FILENAME = "This is a test database file.ini"
-FILEPATH = f"tests/{FILENAME}"
+FILEPATH = f"db/{FILENAME}"
+EXISTFILE = "XDF_Op_TigerTrap.ini"
 
 
 @pytest.fixture
-def sample_db_obj():
-    return INIFile(DBNAME, FILENAME)
+def sample_db_obj() -> INIFile:
+    if os.path.isfile(FILEPATH):
+        os.remove(FILEPATH)
+    ini = INIFile(DBNAME, FILENAME)
+    ini.write(True)
+    return ini
 
 
-def test_should_create_inifile_with_meta_section(sample_db_obj: INIFile):
-    assert isinstance(sample_db_obj, INIFile)
-    assert os.path.isfile(sample_db_obj.file_path) is True
+@pytest.fixture
+def sample_section() -> Section:
+    section = Section("sample_section")
+    section.set("key_one", "value_one")
+    section.set("key_two", "value_two")
+    section.set("key_three", "value_three")
+    return section
+
+
+def test_read_existing_ini_file():
+    section_list: list[str] = []
+    with open(f"db/{EXISTFILE}") as fd:
+        for line in fd.readlines():
+            if (line.find("[")) == 0:
+                section_list.append(line)
+
+    ini = INIFile(DBNAME, EXISTFILE)
+    assert len(section_list) == len(ini.sections) + 1
+
+
+def test_create_inifile_with_meta_section(sample_db_obj: INIFile):
     with open(sample_db_obj.file_path) as fd:
         first_line = fd.readline()
     assert first_line == "[meta]\n"
@@ -29,64 +52,97 @@ def test_should_contain_meta_data(sample_db_obj: INIFile):
         line = fd.readline()
         assert line == "[meta]\n"
         line = fd.readline()
-        assert re.fullmatch('^db_name = ".*"\n$', line) is not None
+        assert re.fullmatch('^db_name=".*"\n$', line) is not None
         line = fd.readline()
         assert (
             re.fullmatch(
-                '^create_date_utc = "[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9|:]{8}"\n', line
+                '^create_date_utc="[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9|:]{8}"\n', line
             )
             is not None
         )
         line = fd.readline()
         assert (
             re.fullmatch(
-                '^create_date_server = "[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9|:]{8}"\n', line
+                '^create_date_server="[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9|:]{8}"\n', line
             )
         ) is not None
     os.remove(sample_db_obj.file_path)
 
 
-def test_should_insert_section_with_content(sample_db_obj: INIFile):
-    title = "test_section"
-    key = "test_key"
-    value = "test_str"
-    section = Section(title)
-    section.set(key, value)
-    sample_db_obj.add(section)
-    found = False
+def test_compare_content_with_existing_ini_file():
+    with open(f"db/{EXISTFILE}") as fd:
+        output = "".join(fd.readlines())
 
-    with open(sample_db_obj.file_path) as fd:
-        for line in fd:
-            if line == f"[{title}]\n":
-                line = next(fd)
-                if line == f'{key} = "{value}"\n':
-                    found = True
-                    break
-    assert found is True
+    ini = INIFile(DBNAME, EXISTFILE)
+    assert output == ini.as_string()
+
+
+def test_add_new_item_to_section(sample_section: Section, sample_db_obj: INIFile):
+    sample_db_obj.add(sample_section)
+    sample_db_obj.write(True)
+    section = sample_db_obj.get("sample_section")
+    output = ""
+    if section is not None:
+        section.set("odd_key", '["odd_value"]')
+        sample_db_obj.add(section)
+        sample_db_obj.write(True)
+        section = sample_db_obj.get('sample_section')
+        if section is not None:
+            output = section.as_string()
+
+    assert (
+        output
+        == """[sample_section]
+key_one="value_one"
+key_two="value_two"
+key_three="value_three"
+odd_key="["odd_value"]"
+"""
+    )
     os.remove(sample_db_obj.file_path)
 
 
-def test_should_update_section(sample_db_obj: INIFile):
-    title = "test_section"
-    key = "test_key"
-    value = "test_str"
-    section = Section(title)
-    section.set(key, value)
-    sample_db_obj.add(section)
+def test_return_all_keys_from_a_section(sample_section: Section):
+    assert sample_section.keys() == ("key_one", "key_two", "key_three")
 
-    new_key = "test_key_2"
-    new_value = "test_str_2"
-    section.set(new_key, new_value)
-    sample_db_obj.add(section)
-    found = False
 
-    with open(sample_db_obj.file_path) as fd:
-        for line in fd:
-            if line == f"[{title}]\n":
-                line = next(fd)
-                line = next(fd)
-                if line == f'{new_key} = "{new_value}"\n':
-                    found = True
-                    break
-    assert found is True
-    # os.remove(sample_db_obj.file_path)
+def test_remove_item_from_section(sample_section: Section):
+    section = sample_section
+    _ = section.remove("key_two")
+    assert (
+        section.as_string()
+        == """[sample_section]
+key_one="value_one"
+key_three="value_three"
+"""
+    )
+
+
+def test_get_a_section(sample_section: Section, sample_db_obj: INIFile):
+    sample_db_obj.add(sample_section)
+    section = sample_db_obj.get("sample_section")
+    output = ""
+
+    if section is not None:
+        output = section.as_string()
+
+    assert (
+        output
+        == """[sample_section]
+key_one="value_one"
+key_two="value_two"
+key_three="value_three"
+"""
+    )
+
+
+def test_delete_section_from_ini_file(sample_section: Section):
+    ini = INIFile(DBNAME, FILENAME)
+    ini.add(sample_section)
+    section = Section("new_section")
+    section.set("new_key", "new_value")
+    ini.add(section)
+    ini.remove("sample_section")
+    ini.write(True)
+    assert ini.sections == ["new_section"]
+    os.remove(ini.file_path)
